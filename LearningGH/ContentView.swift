691980c02ce31d9e94bc8f48ec9dc089609a5af5
joemcmanus93 <125418135+joemcmanus93@users.noreply.github.com>
2023-02-15 01:31:00 +0000

    update content view

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Code Crew!")
+            Text("What's up, Code Crew!")
             
         }
         .padding()

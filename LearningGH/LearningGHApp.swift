83c20844e5959934804fd17a7c44eaafb4ec4b6b
Joe McManus <joemcmanus93@gmail.com>
2023-02-15 01:07:22 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by joe mcmanus on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
